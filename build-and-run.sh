5196d6a10fd9d63597cacbec4971eb824327f082
Cymes1 <mateusz.winiarczyk@outlook.com>
2022-01-24 22:17:39 +0100

    Initial commit

---
+++ build-and-run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# creating directory build if not exist
+mkdir -p build
+cd build || exit
+
+# running cmake
+if ! cmake ..; then
+  exit 1
+fi
+
+# running make
+if ! make; then
+  exit 1
+fi
+
+# running program
+cd build/bin || exit
+./2048
+
+# returning to root directory
+cd ..
---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# creating directory build if not exist
+mkdir -p build
+cd build || exit
+
+# running cmake
+if ! cmake ..; then
+  exit 1
+fi
+
+# running make
+if ! make; then
+    exit 1
+fi
+
+# copy program resources
+if [ -d build/bin/res ]; then
+  rm -r build/bin/res/
+fi
+cp -r ../res build/bin/
+
+# returning to root directory
+cd ..
