5c3d0e7cabf09b22b6257ce0b381c8180757dd5f
Cymes1 <mateusz.winiarczyk@outlook.com>
2022-03-14 06:59:41 +0100

    Added sprite and texture

--- src/state-machine/states/menu-state.cpp
+++ src/state-machine/states/menu-state.cpp
@@ -6,7 +6,11 @@ using namespace Lava::Graphics;
 namespace States
 {
     MenuState::MenuState(GameStateMachine *stateMachine)
-            : State(stateMachine)
+        : State(stateMachine),
+          pikachuTexture("res/texture/pikachu.png"),
+          gerwantTexture("res/texture/happy-gerwant.png"),
+          sprite1(glm::vec3(600, 500, 0), glm::vec3(400, 400, 1), pikachuTexture),
+          sprite2(glm::vec3(1000, 1000, 0), glm::vec3(200, 200, 1), gerwantTexture)
     {}
 
     void MenuState::initialize()
@@ -14,7 +18,8 @@ namespace States
 
     void MenuState::tick()
     {
-        Renderer::getInstance()->draw();
+        Renderer::getInstance()->draw(sprite1);
+        Renderer::getInstance()->draw(sprite2);
     }
 
     void MenuState::renderImGui()
--- src/state-machine/states/menu-state.h
+++ src/state-machine/states/menu-state.h
@@ -2,12 +2,20 @@
 #define MENU_STATE_H
 
 #include <state.h>
+#include <graphics/sprite.h>
+#include <graphics/texture.h>
 #include <game-state-machine.h>
 
 namespace States
 {
     class MenuState : public State<GameStateMachine>
     {
+    private:
+        Lava::Graphics::Texture pikachuTexture;
+        Lava::Graphics::Texture gerwantTexture;
+        Lava::Graphics::Sprite sprite1;
+        Lava::Graphics::Sprite sprite2;
+
     public:
         explicit MenuState(GameStateMachine* stateMachine);
 
