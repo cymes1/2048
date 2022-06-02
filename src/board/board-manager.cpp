5f10c14d8a604bef981c902a03863ad885b82460
Cymes1 <mateusz.winiarczyk@outlook.com>
2022-06-02 20:51:32 +0200

    Added number textures and implement board

--- src/board/board-manager.cpp
+++ src/board/board-manager.cpp
@@ -1,45 +1,63 @@
 #include "board-manager.h"
 #include <graphics/renderer.h>
 #include <input/input-system.h>
-#include <time/time.h>
 
 using namespace Lava::Graphics;
 using namespace Lava::Input;
-using namespace Lava::Time;
 
 namespace Board
 {
     BoardManager::~BoardManager()
     {
         delete sprite;
-        delete texture;
+        delete tex2;
+        delete tex4;
+        delete tex8;
+        delete tex16;
+        delete tex32;
+        delete tex64;
+        delete tex128;
+        delete tex256;
+        delete tex512;
     }
 
     void BoardManager::init()
     {
-        texture = new Texture("res/texture/start-button.png");
-        sprite = new Sprite(glm::vec3(100, 100, 0), glm::vec3(100, 100, 1), *texture);
+        tex2 = new Texture("res/texture/numbers/2.png");
+        tex4 = new Texture("res/texture/numbers/4.png");
+        tex8 = new Texture("res/texture/numbers/8.png");
+        tex16 = new Texture("res/texture/numbers/16.png");
+        tex32 = new Texture("res/texture/numbers/32.png");
+        tex64 = new Texture("res/texture/numbers/64.png");
+        tex128 = new Texture("res/texture/numbers/128.png");
+        tex256 = new Texture("res/texture/numbers/256.png");
+        tex512 = new Texture("res/texture/numbers/512.png");
+        sprite = new Sprite(glm::vec3(50, 50, 0), glm::vec3(100, 100, 1), *tex2);
+
+        for(int i = 0; i < BOARD_SIZE; i++)
+        {
+            for(int j = 0; j < BOARD_SIZE; j++)
+            {
+                board[j][i] = 0;
+            }
+        }
+
+        board[0][0] = 1;
+        board[2][2] = 1;
     }
 
     void BoardManager::tick()
     {
-        if(InputSystem::getInstance()->getKey(GLFW_KEY_UP))
+        for(int i = 0; i < BOARD_SIZE; i++)
         {
-            sprite->translate(glm::vec2(0, 1) * speed * (float)Time::getInstance()->getDeltaTime());
+            for(int j = 0; j < BOARD_SIZE; j++)
+            {
+                if(board[j][i] != 0)
+                {
+                    sprite->setPosition({ 50 + j * 100, 50 + i * 100});
+                    Renderer::getInstance()->draw(*sprite);
+                }
+            }
         }
-        if(InputSystem::getInstance()->getKey(GLFW_KEY_DOWN))
-        {
-            sprite->translate(glm::vec2(0, -1) * speed * (float)Time::getInstance()->getDeltaTime());
-        }
-        if(InputSystem::getInstance()->getKey(GLFW_KEY_RIGHT))
-        {
-            sprite->translate(glm::vec2(1, 0) * speed * (float)Time::getInstance()->getDeltaTime());
-        }
-        if(InputSystem::getInstance()->getKey(GLFW_KEY_LEFT))
-        {
-            sprite->translate(glm::vec2(-1, 0) * speed * (float)Time::getInstance()->getDeltaTime());
-        }
-
-        Renderer::getInstance()->draw(*sprite);
     }
 }
--- src/board/board-manager.h
+++ src/board/board-manager.h
@@ -4,17 +4,28 @@
 #include <graphics/sprite.h>
 #include <graphics/texture.h>
 
+static const char BOARD_SIZE = 3;
+
 namespace Board
 {
     class BoardManager
     {
     private:
-        Lava::Graphics::Texture* texture;
+        Lava::Graphics::Texture* tex2;
+        Lava::Graphics::Texture* tex4;
+        Lava::Graphics::Texture* tex8;
+        Lava::Graphics::Texture* tex16;
+        Lava::Graphics::Texture* tex32;
+        Lava::Graphics::Texture* tex64;
+        Lava::Graphics::Texture* tex128;
+        Lava::Graphics::Texture* tex256;
+        Lava::Graphics::Texture* tex512;
         Lava::Graphics::Sprite* sprite;
 
-        float speed = 500.0f;
+        char board[BOARD_SIZE][BOARD_SIZE];
 
     public:
+        BoardManager() = default;
         ~BoardManager();
 
         void init();
