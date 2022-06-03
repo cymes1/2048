9374252d837a706129c37a47bcfa7d8e1c03b48e
Cymes1 <mateusz.winiarczyk@outlook.com>
2022-06-03 19:35:27 +0200

    Implemented tiles movement

--- src/board/board-manager.cpp
+++ src/board/board-manager.cpp
@@ -42,18 +42,101 @@ namespace Board
             }
         }
 
-        board[0][0] = 1;
-        board[2][2] = 1;
+        board[0][0] = 2;
+        board[2][2] = 2;
+        board[0][2] = 4;
     }
 
     void BoardManager::tick()
     {
+        glm::vec2 dir;
+        if(InputSystem::getInstance()->getKeyDown(GLFW_KEY_RIGHT))
+            dir.x = 1;
+        else if(InputSystem::getInstance()->getKeyDown(GLFW_KEY_LEFT))
+            dir.x = -1;
+        else if(InputSystem::getInstance()->getKeyDown(GLFW_KEY_UP))
+            dir.y = 1;
+        else if(InputSystem::getInstance()->getKeyDown(GLFW_KEY_DOWN))
+            dir.y = -1;
+
+        if(dir.x == 1)
+        {
+            for (int i = BOARD_SIZE - 2; i >= 0; i--)
+            {
+                for (int j = 0; j < BOARD_SIZE; j++)
+                {
+                    char& current = board[i][j];
+                    char& right = board[i + 1][j];
+                    if(current != 0 && (right == 0 || current == right))
+                    {
+                        right += current;
+                        current = 0;
+                    }
+                }
+            }
+        }
+        if(dir.x == -1)
+        {
+            for (int i = 1; i < BOARD_SIZE; i++)
+            {
+                for (int j = 0; j < BOARD_SIZE; j++)
+                {
+                    char& current = board[i][j];
+                    char& left = board[i - 1][j];
+                    if(current != 0 && (left == 0 || current == left))
+                    {
+                        left += current;
+                        current = 0;
+                    }
+                }
+            }
+        }
+        if(dir.y == 1)
+        {
+            for (int i = BOARD_SIZE - 2; i >= 0; i--)
+            {
+                for (int j = 0; j < BOARD_SIZE; j++)
+                {
+                    char& current = board[j][i];
+                    char& up = board[j][i + 1];
+                    if(current != 0 && (up == 0 || current == up))
+                    {
+                        up += current;
+                        current = 0;
+                    }
+                }
+            }
+        }
+        if(dir.y == -1)
+        {
+            for (int i = 1; i < BOARD_SIZE; i++)
+            {
+                for (int j = 0; j < BOARD_SIZE; j++)
+                {
+                    char& current = board[j][i];
+                    char& down = board[j][i - 1];
+                    if(current != 0 && (down == 0 || current == down))
+                    {
+                        down += current;
+                        current = 0;
+                    }
+                }
+            }
+        }
+
         for(int i = 0; i < BOARD_SIZE; i++)
         {
             for(int j = 0; j < BOARD_SIZE; j++)
             {
-                if(board[j][i] != 0)
+                if(board[j][i] == 4)
                 {
+                    sprite->setTexture(*tex4);
+                    sprite->setPosition({ 50 + j * 100, 50 + i * 100});
+                    Renderer::getInstance()->draw(*sprite);
+                }
+                else if(board[j][i] == 2)
+                {
+                    sprite->setTexture(*tex2);
                     sprite->setPosition({ 50 + j * 100, 50 + i * 100});
                     Renderer::getInstance()->draw(*sprite);
                 }
