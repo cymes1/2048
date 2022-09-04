ae1759882e5e597177692a0c8f97479090521bf1
Cymes1 <mateusz.winiarczyk@outlook.com>
2022-09-04 18:54:12 +0200

    Add entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,81 +1,17 @@
-#define NDEBUG
-
-#include <cstring>
-
-#include <graphics/window.h>
-#include <graphics/vulkan-context.h>
-#include <audio/audio-system.h>
-/*
-#include <input/input-system.h>
-#include <time/time.h>
-#include <game-state-machine.h>
-*/
-
-using namespace Lava::Graphics;
-using namespace Lava::Audio;
-/*
-using namespace Lava::Input;
-using namespace Lava::Time;
-using namespace States;
-*/
+#include <lava.h>
 
 using namespace Lava;
 
-void updateInput();
-bool shouldInitializeVulkan(int args, char** argv);
-
-int main(int args, char** argv)
+class Game : public LavaGame
 {
-//    Game game;
-//    GameStateMachine stateMachine;
-    bool initializeVulkan = shouldInitializeVulkan(args, argv);
-    Window window("2048", initializeVulkan);
+public:
+    Game(int args, char** argv)
+        : LavaGame(args, argv)
+    {}
+};
 
-    if(initializeVulkan)
-        VulkanContext::getInstance()->initialize(window);
-
-//    InputSystem::getInstance()->initialize(window);
-//    Time::getInstance()->initialize();
-    AudioSystem::getInstance()->initialize();
-//    stateMachine.createNewState<BootState>();
-    while(!glfwWindowShouldClose(window.getHandle()))
-    {
-        glfwPollEvents();
-        updateInput();
-        AudioSystem::getInstance()->tick();
-        if(glfwGetKey(window.getHandle(), GLFW_KEY_L) == GLFW_PRESS)
-            AudioSystem::getInstance()->playSound();
-
-//        Time::getInstance()->tick();
-        if(initializeVulkan)
-            VulkanContext::getInstance()->tick();
-//        glClear(GL_COLOR_BUFFER_BIT);
-
-//        stateMachine.tick();
-//        stateMachine.renderImGui();
-//        glfwSwapBuffers(window.getHandle());
-
-/*
-        if(InputSystem::getInstance()->getShouldExit())
-            glfwSetWindowShouldClose(window.getHandle(), 1);
-*/
-    }
-//    stateMachine.deinitialize();
-    if(initializeVulkan)
-        VulkanContext::getInstance()->waitForVulkan();
-    return 0;
+LavaGame* Lava::createGame(int args, char** argv)
+{
+    return new Game(args, argv);
 }
 
-void updateInput()
-{
-/*
-    InputSystem::getInstance()->tickInput();
-    glfwPollEvents();
-*/
-}
-
-bool shouldInitializeVulkan(int args, char** argv)
-{
-    return true;
-    return args > 1 && strcmp(argv[1], "-vulkan") == 0;
-}
